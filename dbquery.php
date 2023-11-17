bf1bfaae7c67e638ba77daeaec0245fd6a044366
Yifei Wang <yifeiwang9119@gmail.com>
2023-11-17 11:58:39 -0800

    filter dates

--- dbquery.php
+++ dbquery.php
@@ -1,8 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
+//set up connection to server
 $dbserver = "localhost";
 $dbuser = "root";
 $dbpass = "";
@@ -14,12 +11,12 @@ if ($db->connect_errno) {
     exit();
 }
 
-$query_str = "SELECT *
-FROM orders INNER JOIN orderdetails ON
-orders.orderNumber = orderdetails.orderNumber
-";
-// Check if orderNumber is set in the request and sanitize it
+
+// Check if orderNumber, checkbox columns, start and end date is set in the request and sanitize it
 $orderNumber = isset($_REQUEST["orderNumber"]) ? $db->real_escape_string($_REQUEST["orderNumber"]) : '';
+$orderDateStart = isset($_REQUEST["start"]) ? $db->real_escape_string($_REQUEST["start"]) : '';
+$orderDateEnd = isset($_REQUEST["end"]) ? $db->real_escape_string($_REQUEST["end"]) : '';
+
 if(isset($_REQUEST['columns'])) {
     $checkbox = ($_REQUEST['columns']);
 }
@@ -27,10 +24,11 @@ else {
     $checkbox = [];
 }
 
-$res = $db->query($query_str);
+//select data in SQL database
 $orderNumber_str = "SELECT DISTINCT orders.orderNumber, orderdetails.orderNumber FROM orders JOIN orderdetails ON orders.orderNumber = orderdetails.orderNumber ORDER BY orders.orderNumber";
 $orderDate_str = "SELECT DISTINCT orderDate FROM orders";
 
+//display form
 echo "<style>section{display:flex;} .part1, .part2{margin-left:2rem;} form{display:flex;flex-direction:column;} .checkbox{display:block;} </style>";
 echo "<h1>Query</h1>";
 echo "<form action='dbquery.php' method='POST'>";
@@ -39,26 +37,42 @@ echo "<section>";
 echo "<div class='part1'>";
 echo "<h2>Select Order Parameters</h2>";
 
-// echo "Order Number: <input type='text' name='orderNumber' value='" .($orderNumber) . "'><br>";
+//display all order numbers from the database
 echo "Order Number: <select name=orderNumber>";
 echo "<option selected></option>";
 foreach ($db->query($orderNumber_str) as $row) {
     echo "<option value=\"" . $row['orderNumber'] . "\"";
+    //match order number with selected order number and display as selected
     if ($orderNumber == $row['orderNumber'])
         echo 'selected';
     echo ">" . $row['orderNumber'] . "</option>";
 }
 echo "</select><br>";
-// echo "Order Date (YYYY-MM-DD): <input type='text' name='orderDateFrom' placeholder='from'> to <input type='text' name='orderDateTo' placeholder='to'><br>";
-echo "Order Date (YYYY-MM-DD): <br>from: <select name=orderDate value=''>";
+
+//display all order dates from the database
+echo "Order Date (YYYY-MM-DD): <br>from: <select name=start value=''>";
+echo "<option selected></option>";
 foreach ($db->query($orderDate_str) as $row) {
-    echo "<option value=$row[orderDate]>$row[orderDate]</option>";
+    echo "<option value=\"" . $row['orderDate'] . "\"";
+    
+    //match order date with selected order date and display as selected
+    if ($orderDateStart == $row['orderDate'])
+        echo 'selected';
+    echo ">" . $row['orderDate'] . "</option>";
+
 }
 
+//display all order dates from the database
 echo "</select>";
-echo "to: <select name=orderDate value=''>";
+echo "to: <select name=end value=''>";
+echo "<option selected></option>";
 foreach ($db->query($orderDate_str) as $row) {
-    echo "<option value=$row[orderDate]>$row[orderDate]</option>";
+    echo "<option value=\"" . $row['orderDate'] . "\"";
+    
+    //match order date with selected order date and display as selected
+    if ($orderDateEnd == $row['orderDate'])
+        echo 'selected';
+    echo ">" . $row['orderDate'] . "</option>";
 }
 echo "</select><br>";
 echo "<input type='submit' name='submit' value='Submit'>";
@@ -67,6 +81,8 @@ echo "</div>";
 echo "<div class='part2'>";
 echo "<h2>Select Columns to Display</h2>";
 
+//display the columns checkboxes
+//if selected mark the checkbox as checked
 echo "<label class='checkbox'><input type='checkbox' name='columns[]' value='orderNumber' ";
 foreach($checkbox as $selected) {
     if ( $selected == 'orderNumber') {
@@ -123,25 +139,32 @@ echo "</form>";
 
 
 
+//select range from selected start and end date
+$query_str2="SELECT * FROM orders INNER JOIN orderdetails ON orders.orderNumber = orderdetails.orderNumber INNER JOIN products on products.productCode = orderdetails.productCode WHERE orderDate BETWEEN '$orderDateStart' AND '$orderDateEnd'";
 
-
+//select the chosen order number
 $query_str1 = "SELECT * FROM orders INNER JOIN orderdetails ON orders.orderNumber = orderdetails.orderNumber INNER JOIN products on products.productCode = orderdetails.productCode WHERE orders.orderNumber =?";
 if (!isset($_REQUEST["submit"]))
     exit();
 else {
-    $orderNumber = $_REQUEST['orderNumber'];
-
+    //prepare connection
     $stmt = mysqli_prepare($db, $query_str1);
     if (!$stmt) {
         echo mysqli_error($db);
     }
-
+    //bind the selected order number with the ?
     mysqli_stmt_bind_param($stmt, "i", $orderNumber);
     mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
 
+    //get the results from the order number 
+    //get the results from the order dates
+    $result = mysqli_stmt_get_result($stmt);
+    $result1 = mysqli_query($db, $query_str2);
+
+    //display resulting table
             echo "<table border='1' style='text-align:center;'><tbody>";
             echo "<tr style='background-color:lightgrey;'>";
+            //show the selected columns
             for ($i = 0; $i < count($checkbox); $i++) {
                 if ($checkbox[$i] == 'orderNumber') {
                     echo "<th>Order Number</th>";
@@ -168,10 +191,12 @@ else {
 
             }
             echo '</tr>';
-            while ($row = mysqli_fetch_assoc($result)) {
+
+            //fetching rows from either the results from order date or order number
+            while ($row = mysqli_fetch_assoc($result) or $row = mysqli_fetch_assoc($result1)) {
                 echo '<tr>';
                 foreach ($checkbox as $selected) {
-
+                    //display rows according to the selected columns
                     if ($selected == 'orderNumber') {
                         echo "<td>" . $row["orderNumber"] . "</td>";
                     }
@@ -202,11 +227,7 @@ else {
     echo "</tbody></table>";
 
     mysqli_free_result($result);
-    // echo $query_str1;
-// $res = $db->query($query_str1);
 
-    // echo "<p>Number of orders: " . $res->num_rows . "</p>";
-// $res->free_result();
     $db->close();
 }
 ?>
